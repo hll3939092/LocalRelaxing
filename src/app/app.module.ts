b37894670b1eef656d1371920c3dbf5aaf6373b8
xupan <hll3939092@gmail.com>
2017-11-17 16:31:02 +0800

    first commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RollbarComponent } from './com/rollbar/rollbar.component';
+import { HomeComponent } from './com/home/home.component';
+import { LinkComponent } from './com/link/link.component';
+
+
+export const appRoutes: Routes = [
+  { path: 'Home', component: HomeComponent },
+  { path: 'Link',      component: LinkComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RollbarComponent,
+    HomeComponent,
+    LinkComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
